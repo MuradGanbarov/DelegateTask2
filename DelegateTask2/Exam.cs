8036b467ef14cf27a39e7d478ca52b7ba5612750
MuradGanbarov <145640606+MuradGanbarov@users.noreply.github.com>
2023-10-24 09:13:54 +0400

    Update Exam.cs
    Because, I didn't save before my changes.

--- DelegateTask2/Exam.cs
+++ DelegateTask2/Exam.cs
@@ -14,16 +14,21 @@ namespace DelegateTask2
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
 
-        
+        public Exam(string subject, double examDuration, DateTime startDate)
+        {
+            Subject = subject;
+            ExamDuration = examDuration;
+            StartDate = startDate;
+
+            DateTime endDate = startDate.AddHours(examDuration);
+
+            EndDate = endDate;
+        }
 
         public override string ToString()
         {
-            return Subject;
-            
+            return $"Subject name: {Subject}, Exam Duration: {ExamDuration} hours, StartDate: {StartDate}, EndDate{EndDate}";
         }
-
         
-        
-
     }
 }
